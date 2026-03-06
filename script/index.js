8bdb465839e90cd3062913cc18bf74f5bfa84a90
Nathan-Front <jonathantindungan3@gmail.com>
2026-03-06 20:37:28 +0900

    redesigned nav and upper panel

--- script/index.js
+++ script/index.js
@@ -37,9 +37,78 @@ async function fetchNavigation(){
     const resFooter = await fetch("./footer/footer.html");
     const footerlHTML = await resFooter.text();
     body.insertAdjacentHTML("beforeend", footerlHTML);
+   animateUpperImages();
 }
 
 async function initAsync(){
     await fetchNavigation();
+    categoryToggle();
+    animateUpperImages();
 }
 document.addEventListener("DOMContentLoaded", initAsync);
+
+
+
+function categoryToggle(){
+    //Function for the news dropdown buttons
+    const newsBtn = document.querySelectorAll('.new-item-display-button');
+    newsBtn.forEach(function (btnEl){
+        btnEl.addEventListener('click', (e) => {
+        //Remove the newsItemActive class name and add it to the next clicked button
+        document.querySelector('.newsItemActive').classList.remove('newsItemActive');
+        btnEl.classList.add('newsItemActive');
+        });
+    });
+    //USe data- and target the related item with Id name same as the data-target value
+    document.querySelectorAll('button[data-target]').forEach(function (el){
+        el.addEventListener('click', () =>{
+            //Get list of the items to be displayed one by one
+            let newsContainer = document.getElementsByClassName('new-item-panel');
+            for(let i = 0; i < newsContainer.length; i++){
+                newsContainer[i].style.display = 'none';
+            }
+            //Target the Id name with the same data-target value of the clicked button
+            document.getElementById(el.getAttribute('data-target')).style.display = "flex";
+        });
+    });
+
+    //For dropdown contents of category container
+    const categoryImgBtn = document.querySelectorAll('.category-images');
+    categoryImgBtn.forEach((btn)=>{
+        btn.addEventListener('click', ()=>{
+            const clickedBtn = btn.closest('.category-images');
+            const displayBtnMssg = clickedBtn.querySelector('.dropdown-buttons').textContent;
+            alert('Link to ' + displayBtnMssg + ' page');
+        });
+    });
+
+    const dailyImgBtn = document.querySelectorAll('.daily-images');
+    dailyImgBtn.forEach((btn)=>{
+        btn.addEventListener('click', ()=>{
+            const clickedBtn = btn.closest('.daily-images');
+            const displayBtnMssg = clickedBtn.querySelector('.dropdown-buttons').textContent;
+            alert('Link to ' + displayBtnMssg + ' page');
+        });
+    }); 
+}
+
+function animateUpperImages(){
+    let images = document.querySelectorAll(".upper-carousel");
+    if(images.length === 0) return null;
+    let imageIndex = 0;
+    images[imageIndex].classList.add("upper-display");
+
+    function imageAnimate(){
+        const current = images[imageIndex];
+        current.classList.remove("upper-display");
+        current.classList.add("upper-hide");
+        imageIndex = (imageIndex + 1) % images.length;
+        const next = images[imageIndex];
+        next.classList.remove("upper-hide");
+        next.classList.add("upper-display");
+        setTimeout(() => {
+        current.classList.remove("upper-hide");
+        }, 600);
+    }
+    setInterval(imageAnimate, 4000);
+}
