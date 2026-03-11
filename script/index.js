71187974471da865156b99a07af79cee9858ca21
Nathan-Front <jonathantindungan3@gmail.com>
2026-03-11 22:23:46 +0900

    added user logged indicator and mobileUserInfo html on mobile viewport

--- script/index.js
+++ script/index.js
@@ -54,6 +54,10 @@ async function fetchPopupPanels(){
     const resCreateAccForm = await fetch("./createForm/createAccountForm.html");
     const createAccFormHTML = await resCreateAccForm.text();
     body.insertAdjacentHTML("beforeend", createAccFormHTML);
+
+    const resUserInfo = await fetch("./loginForm/mobileUserInfo.html");
+    const userInfoHTML = await resUserInfo.text();
+    body.insertAdjacentHTML("beforeend", userInfoHTML);
 }
 
 async function initAsync(){
@@ -69,9 +73,9 @@ async function initAsync(){
    
     mobileNewsContent();
     mobileCart();     
-    loginPage(); 
+    userDisplay(); 
 
-    signInUser();
+    loginCreateForm();
 
     createAccount();
     loginAccount();
@@ -155,24 +159,35 @@ function animateUpperImages() {
     setInterval(imageAnimate, 7000);
 }
 
-function loginPage(){
+function userDisplay(){
     const mobileLoginBtn = document.getElementById("mobile-login-button");
     const formWrap = document.querySelector(".mobile-form");
+    const userInfo = document.querySelector(".logged-in-user-info-panel");
     mobileLoginBtn.addEventListener("click", ()=>{
-         const isOpen = formWrap.classList.contains("showLogin");
-         closeAll();
-        if (!isOpen) {
+            const savedUser = JSON.parse(localStorage.getItem("loggedUser"));
+            const isUserInfoOpen = userInfo.classList.contains("showUserInfo");
+            const isOpen = formWrap.classList.contains("showLogin");
+            closeAll();
+            updateUserIcon();
+            if (!isUserInfoOpen && savedUser) {
+                userInfo.classList.add("showUserInfo");
+                document.body.classList.add("no-scroll");
+                mobileLoginBtn.src = savedUser.indicator;
+            }
+            else if (!savedUser && !isOpen) {
+               
                 formWrap.classList.add("showLogin");
                 document.body.classList.add("no-scroll");
-                mobileLoginBtn.src = "../images/logo/profile-user-svgrepo-com-blue.svg"
-            }
+                mobileLoginBtn.src = "../images/logo/profile-user-svgrepo-com-blue.svg"  
+            } 
+            
     });
+    
 }
 function returnHome(){
     const homeBtn = document.getElementById("mobile-home-button");
     homeBtn.addEventListener("click", ()=>{
         window.location.href = "index.html";
-        location.reload();
     });
 }
 
@@ -234,15 +249,15 @@ function mobileNewsContent(){
 function closeAll(){
     const formWrap = document.querySelector(".mobile-form");
     const overlay = document.getElementById("overlay");
-    const mobileLoginBtn = document.getElementById("mobile-login-button");
     const registerForm = document.querySelector(".create-accnt-page");
     const cart = document.querySelector(".check-out-cart-panel");
     const displayNews = document.getElementById("toggle-mobile-news");
     const burgerOpenBtn = document.getElementById("burger-open-button");
+    const userInfo = document.querySelector(".logged-in-user-info-panel");
     if(formWrap){
         formWrap.classList.remove("showLoginDesk");
         formWrap.classList.remove("showLogin");
-        mobileLoginBtn.src = "../images/logo/profile-user-svgrepo-com-gray.svg";
+        userInfo.classList.remove("showUserInfo");
     }
     if(registerForm){
         registerForm.classList.remove("showCreateAccForm");
@@ -261,19 +276,37 @@ function closeAll(){
     }
     document.body.classList.remove("no-scroll");
 }
+function updateUserIcon(){
+    const mobileLoginBtn = document.getElementById("mobile-login-button");
+    const savedUser = JSON.parse(localStorage.getItem("loggedUser"));
+    if(savedUser){
+        mobileLoginBtn.src = savedUser.indicator;
+    }else{
+        mobileLoginBtn.src = "../images/logo/profile-user-svgrepo-com-gray.svg";
+         document.body.classList.remove("no-scroll");
+    }
+
+}
 
 
-
-//login form for desktop
-function signInUser(){
+//Displaying login/create Form
+function loginCreateForm(){
     const signInBtn = document.getElementById("sign-in");
     const formWrap = document.querySelector(".mobile-form");
     signInBtn.addEventListener("click", ()=>{
+        const guest = document.getElementById("user-to-log");
+        if(guest.textContent !== "Guest"){
+            alert("Already logged in"); 
+            return;
+        }
         const isOpen = formWrap.classList.contains("showLoginDesk");
         closeAll();
+        const inputUser = document.getElementById("input-user-name");
+        const rememberMe = JSON.parse(localStorage.getItem("rememberUserName"));
         if(!isOpen){
             formWrap.classList.add("showLoginDesk");
             document.body.classList.add("no-scroll");
+            inputUser.value = rememberMe;
             const overlay = document.getElementById("overlay");
             overlay.classList.toggle("cover");
         }
@@ -281,6 +314,21 @@ function signInUser(){
 
     const registerBtn = document.getElementById("register");
     const registerForm = document.querySelector(".create-accnt-page");
+    const toCreateForm = document.getElementById("redirect-to-create-form");
+    toCreateForm.addEventListener("click", ()=>{
+        closeAll();
+        const mobileLoginBtn = document.getElementById("mobile-login-button");
+        mobileLoginBtn.src = "../images/logo/profile-user-svgrepo-com-blue.svg";
+        const isOpen = registerForm.classList.contains("showCreateAccForm");
+        if(!isOpen){
+            registerForm.classList.add("showCreateAccForm");
+            document.body.classList.add("no-scroll");
+            const overlay = document.getElementById("overlay");
+            overlay.classList.toggle("cover");
+        }
+        
+    });
+
     registerBtn.addEventListener("click", ()=>{
         const isOpen = registerForm.classList.contains("showCreateAccForm");
         closeAll();
@@ -298,5 +346,19 @@ function signInUser(){
             closeAll();
         });
     });
+    toggleEye();
+}
+
+function toggleEye(){
+  document.addEventListener("click", (e) => {
+    const eye = e.target.closest(".toggle-password"); //Shared className of the eye
+    if(!eye) return;
+    const passwordField = document.getElementById(eye.dataset.target); //Use data- to target the id of the input field of the toggled eye
+    if(!passwordField) return;
+    const isPassword = passwordField.type === "password"; //Does the input field type equal to password
+    passwordField.type = isPassword ? "text" : "password"; //If password then change to text other wise as is
+    eye.classList.toggle("fa-eye"); //For initial existance of the eye
+    eye.classList.toggle("fa-eye-slash");
+  });
 }
 
--- script/userForm.js
+++ script/userForm.js
@@ -1,4 +1,4 @@
-
+//Creating account function
 function createAccount(){
     const createForm = document.getElementById("create-accnt-btn");
     createForm.addEventListener("submit", (e)=>{
@@ -30,10 +30,34 @@ function createAccount(){
         }
         users.push(userRegister);
         localStorage.setItem("registeredUsers", JSON.stringify(users));
-        createForm.reset();
+        
+        const savedUser = JSON.parse(localStorage.getItem("loggedUser"));
+        if(savedUser){
+            alert("Account created");
+            window.location.href = "index.html";
+            return;
+        }else{
+            alert("Account created");
+            const formWrap = document.querySelector(".mobile-form");
+            const isOpen = formWrap.classList.contains("showLoginDesk");
+            closeAll();
+            if(!isOpen){
+                formWrap.classList.add("showLoginDesk");
+                document.body.classList.add("no-scroll");
+                const overlay = document.getElementById("overlay");
+                overlay.classList.toggle("cover");
+            }
+        }
+
+    });
+
+    const cancelInput = document.getElementById("cancel-create-input");
+    cancelInput.addEventListener("click", ()=>{
+        document.getElementById("create-accnt-btn").reset();
     });
 }
 
+//Loggin account function
 function loginAccount(){
     const loginForm = document.getElementById("mobile-form-submit");
     loginForm.addEventListener("submit", (e)=>{
@@ -42,25 +66,46 @@ function loginAccount(){
         const inputPass = document.getElementById("input-user-pass");
         const users = JSON.parse(localStorage.getItem("registeredUsers")) || [];
         const existUser = users.find(user => user.userName === inputUser.value && user.userPassword === inputPass.value);
+        const userLogIndicator = "../images/logo/profile-user-svgrepo-com-green.svg";
         if(existUser){
-            localStorage.setItem("loggedUser", JSON.stringify(existUser));
+            localStorage.setItem("loggedUser", JSON.stringify({
+                user: existUser,
+                indicator: userLogIndicator
+            }));
             const savedUser = JSON.parse(localStorage.getItem("loggedUser"));
+            const rememberMe = document.getElementById("mobile-remember-me");
+            if(rememberMe.checked){
+                localStorage.setItem('rememberUserName', JSON.stringify(inputUser.value));
+            }else{
+                localStorage.removeItem('rememberUserName');
+            }
             if(savedUser){
-                document.getElementById("user-to-log").textContent = savedUser.userName;
+                document.getElementById("user-to-log").textContent = savedUser.user.userName;
+                document.getElementById("mobile-user-to-log").textContent = savedUser.user.userName;
+                document.getElementById("mobile-login-button").src = savedUser.indicator;
             }
             closeAll();
         }else{
             alert("Invalid username or password");
         }
     });
+
+    const cancelInput = document.getElementById("mobile-cancel-input");
+    cancelInput.addEventListener("click", ()=>{
+        document.getElementById("mobile-form-submit").reset();
+    });
 }
-//Use to restore the logged in user
+//Use to restore the logged in user on reload
 async function restoreLoggedUser(){
     const savedUser = JSON.parse(localStorage.getItem("loggedUser"));
     if(!savedUser) return;
     const displayUser = document.getElementById("user-to-log");
+    const displayUserMob =  document.getElementById("mobile-user-to-log");
+    const logIndicator = document.getElementById("mobile-login-button");
     if(displayUser) {
         displayUser.textContent = savedUser.userName;
+        displayUserMob.textContent = savedUser.user.userName;
+        logIndicator.src = savedUser.indicator;
     }
 }
 
